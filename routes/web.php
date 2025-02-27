e0d8d6872dac2343c5d798b12c03cac8625a49f0
AlvinAditiya117 <ryuzavin@gmail.com>
2025-02-28 06:53:32 +0700

    Tugas_Praktikum_PWL_Jobsheet_2

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+
 use App\Http\Controllers\SalesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,10 +19,8 @@ use App\Http\Controllers\SalesController;
 */
 
 
-// Halaman Home
 Route::get('/', [HomeController::class, 'index']);
 
-// Halaman Products dengan prefix "/category"
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
@@ -28,8 +28,5 @@ Route::prefix('category')->group(function () {
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
 
-// Halaman User dengan parameter
 Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-// Halaman Penjualan
 Route::get('/sales', [SalesController::class, 'index']);
